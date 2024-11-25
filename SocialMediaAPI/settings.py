9409859b3e0c54893deb6a439e15be8e56732fad
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-11-25 17:17:25 +0200

    Created user app and added to INSTALLED_APPS

--- SocialMediaAPI/settings.py
+++ SocialMediaAPI/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "social_media"
+    "social_media",
+    "user",
 ]
 
 MIDDLEWARE = [
