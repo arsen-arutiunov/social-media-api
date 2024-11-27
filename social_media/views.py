e3ae21d4379f143fe3950193d51b204c9c6d5fa8
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-11-27 16:09:18 +0200

    Deleted commentaries

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from rest_framework import mixins, permissions, viewsets
 
-# Create your views here.
+
+from social_media.models import (
+    Post,
+    Profile,
+    Follow,
+    Like,
+    Comment, Hashtag,
+)
+from social_media.serializers import (
+    ProfileSerializer,
+    FollowSerializer,
+    PostSerializer,
+    LikeSerializer,
+    CommentSerializer, HashtagSerializer,
+)
+
+
+
+class ProfileViewSet(viewsets.GenericViewSet,
+                     mixins.RetrieveModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.ListModelMixin,
+                     mixins.UpdateModelMixin):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Profile.objects.filter(user=self.request.user)
+
+    def get_object(self):
+        return self.request.user.profile
+
+
+class FollowViewSet(viewsets.GenericViewSet,
+                    mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    mixins.DestroyModelMixin):
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Follow.objects.filter(follower=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(follower=self.request.user)
+
+    def get_object(self):
+        following_id = self.kwargs.get("pk")
+        return Follow.objects.get(follower=self.request.user,
+                                  following_id=following_id)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Post.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HashtagViewSet(viewsets.ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class LikeViewSet(viewsets.GenericViewSet,
+                  mixins.CreateModelMixin,
+                  mixins.ListModelMixin,
+                  mixins.DestroyModelMixin):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Like.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
