07856780e1a2944d1b3760efb9f1feb87b5a27a6
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-11-27 20:05:26 +0200

    Update urls

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from social_media.views import (
+    ProfileViewSet,
+    PostViewSet,
+    HashtagViewSet,
+    LikeViewSet,
+    CommentViewSet,
+    FollowViewSet,
+)
 
 router = routers.DefaultRouter()
+router.register(r"profiles", ProfileViewSet, basename="profile")
+router.register(r"follow", FollowViewSet, basename="follow")
+router.register(r"posts", PostViewSet, basename="post")
+router.register(r"hashtags", HashtagViewSet, basename="hashtag")
+router.register(r"likes", LikeViewSet, basename="like")
+router.register(r"comments", CommentViewSet, basename="comment")
 
 urlpatterns = [path("", include(router.urls))]
 
