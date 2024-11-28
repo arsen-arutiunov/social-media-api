51fc5e4f69aae3b9007cba08064eb1ec2a676759
Arsen Arutiunov <arsen.test.mess@gmail.com>
2024-11-28 17:16:54 +0200

    Removed doc endpoint from urlpatterns

--- SocialMediaAPI/urls.py
+++ SocialMediaAPI/urls.py
@@ -36,9 +36,4 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path(
-        "api/doc/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc"
-    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
